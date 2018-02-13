fa3cb1e634f9ff045c46f49ec3a73cfa9c9b5da3
kazuki doumae <donmainakanaideyo@gmail.com>
2018-02-13 12:02:27 +0900

    リファクタリング

--- app/src/main/java/com/example/doumaekazuki/slideapp/EditSurfaceView.java
+++ app/src/main/java/com/example/doumaekazuki/slideapp/EditSurfaceView.java
@@ -8,6 +8,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorMatrix;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
@@ -25,8 +26,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Deque;
+import java.util.List;
 import java.util.Random;
 
 
@@ -55,7 +61,8 @@ public class EditSurfaceView extends SurfaceView implements SurfaceHolder.Callba
 
     // 途中経過
     private Deque<Path> mUndoStack = new ArrayDeque<Path>();
-    private Deque<Path> mRedoStack = new ArrayDeque<Path>();
+    List<Path> pathList;
+    List<Paint> paintList;
 
     // テスト
     Bitmap test;
@@ -77,9 +84,9 @@ public class EditSurfaceView extends SurfaceView implements SurfaceHolder.Callba
         // 編集画像
         imagePass = pass;
         // 線色初期設定
-        r = 255;
-        g = 255;
-        b = 255;
+        r = 0;
+        g = 0;
+        b = 0;
         // 線幅初期設定
         sw = 26;
 
@@ -122,40 +129,35 @@ public class EditSurfaceView extends SurfaceView implements SurfaceHolder.Callba
 
     @Override
     public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+
+        // サイズ取得
         sizeX = width;
         sizeY = height;
+
+        // Path,Paintリスト初期化
+        pathList = new ArrayList<Path>();
+        paintList = new ArrayList<Paint>();
         clearLastDrawBitmap();
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        testBitmap.recycle();
         test.recycle();
     }
 
     private void clearLastDrawBitmap() {
-
-        if (testBitmap == null) {
-            // Bitmap変換
-            uri = Uri.parse(imagePass);
-            try {
-                testBitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(),uri);
-                //test = Bitmap.createScaledBitmap(testBitmap.copy(Bitmap.Config.ARGB_8888,true), testBitmap.getWidth(), testBitmap.getHeight(), false);
-                test = Bitmap.createScaledBitmap(testBitmap.copy(Bitmap.Config.ARGB_8888,true), sizeX, sizeY, true);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        // Bitmap変換
+        uri = Uri.parse(imagePass);
+        try {
+            testBitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(),uri);
+            test = Bitmap.createScaledBitmap(testBitmap.copy(Bitmap.Config.ARGB_8888,true), sizeX, sizeY, true);
+            testBitmap.recycle();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        if (testCanvas == null) {
-            testCanvas = new Canvas(test);
-        }
-
-        //test = Bitmap.createScaledBitmap(test, testCanvas.getWidth(), testCanvas.getHeight(), false);
+        testCanvas = new Canvas(test);
         drawCanvas =  holder.lockCanvas();
-        // 位置調整
-        drawCanvas.translate((drawCanvas.getWidth()-test.getWidth())/2,(drawCanvas.getHeight()-test.getHeight())/2);
-        drawCanvas.drawBitmap(test,0,0,null);
+        drawingImage();
         holder.unlockCanvasAndPost(drawCanvas);
     }
 
@@ -185,75 +187,48 @@ public class EditSurfaceView extends SurfaceView implements SurfaceHolder.Callba
     }
     private void onTouchUp(float x, float y) {
         mPath.lineTo(x, y);
-
         drawLine(mPath);
         testCanvas.drawPath(mPath, mPaint);
-
-        mUndoStack.addLast(mPath);
-        mRedoStack.clear();
+        pathList.add(mPath);
+        paintList.add(mPaint);
     }
 
 
     // 描画処理
     private void drawLine(Path path) {
-        // ロックしてキャンバスを取得
         drawCanvas = holder.lockCanvas();
         // キャンバスをクリア
         drawCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
-        // 位置調整
-        drawCanvas.translate((drawCanvas.getWidth()-test.getWidth())/2,(drawCanvas.getHeight()-test.getHeight())/2);
-
-        // 前回描画したビットマップをキャンバスに描画
-        drawCanvas.drawBitmap(test, 0, 0, null);
+        drawingImage();
         // パスを描画
         drawCanvas.drawPath(path, mPaint);
-        // ロックを外す
         holder.unlockCanvasAndPost(drawCanvas);
     }
 
     public void undo() {
-        if (mUndoStack.isEmpty()) {
-            return;
-        }
-        // undoスタックからパスを取り出し、redoスタックに格納します。
-        Path lastUndoPath = mUndoStack.removeLast();
-        mRedoStack.addLast(lastUndoPath);
-        // ロックしてキャンバスを取得します。
-        Canvas canvas = holder.lockCanvas();
+        if (pathList.size() != 0 && paintList.size() != 0) {
 
-        // キャンバスをクリアします。
-        canvas.drawColor(0, PorterDuff.Mode.CLEAR);
-
-        // 描画状態を保持するBitmapをクリアします。
-        //clearLastDrawBitmap();
-        // パスを描画します。
-        for (Path path : mUndoStack) {
-            canvas.drawPath(path, mPaint);
-            testCanvas.drawPath(path, mPaint);
+            // 最後のPath,Paintを削除
+            pathList.remove(pathList.size() - 1);
+            paintList.remove(paintList.size() - 1);
+            // Canvas初期化
+            clearLastDrawBitmap();
+            drawCanvas = holder.lockCanvas();
+            drawingImage();
+            // パスを描画
+            for (int i = 0; i < pathList.size(); i++) {
+                drawCanvas.drawPath(pathList.get(i), paintList.get(i));
+                testCanvas.drawPath(pathList.get(i), paintList.get(i));
+            }
+            holder.unlockCanvasAndPost(drawCanvas);
         }
-        // ロックを外します。
-        holder.unlockCanvasAndPost(canvas);
     }
 
-    public void redo() {
-        if (mRedoStack.isEmpty()) {
-            return;
-        }
-        // redoスタックからパスを取り出し、undoスタックに格納します。
-        Path lastRedoPath = mRedoStack.removeLast();
-        mUndoStack.addLast(lastRedoPath);
-        // パスを描画します。
-        drawLine(lastRedoPath);
-//        testCanvas.drawPath(lastRedoPath, mPaint);
-    }
-
-    public void reset() {
-        mUndoStack.clear();
-        mRedoStack.clear();
-        //clearLastDrawBitmap();
-        Canvas canvas = holder.lockCanvas();
-        canvas.drawColor(0, PorterDuff.Mode.CLEAR);
-        holder.unlockCanvasAndPost(canvas);
+    // 画像描画
+    private void drawingImage (){
+        // 位置調整
+        drawCanvas.translate((drawCanvas.getWidth()-test.getWidth())/2,(drawCanvas.getHeight()-test.getHeight())/2);
+        drawCanvas.drawBitmap(test,0,0,null);
     }
 
     // 画像保存
@@ -262,7 +237,8 @@ public class EditSurfaceView extends SurfaceView implements SurfaceHolder.Callba
         File dataDir = new File(Environment.getExternalStorageDirectory(), "sampleDir");
         dataDir.mkdirs();
         String fileNumber = String.valueOf(new Random().nextInt(100000000));
-        File filePath = new File(dataDir,"sample"+fileNumber+".jpg");
+        //File filePath = new File(dataDir,"sample"+fileNumber+".jpg");
+        File filePath = new File(dataDir,getNowData()+".jpg");
         OutputStream os= null;
         try {
             os = new FileOutputStream(filePath);
@@ -277,10 +253,15 @@ public class EditSurfaceView extends SurfaceView implements SurfaceHolder.Callba
             values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
             values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
             values.put(MediaStore.MediaColumns.DATA, filePath.getAbsolutePath());
-
             getContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static String getNowData() {
+        final DateFormat df = new SimpleDateFormat("yyy/mm/dd HH:mm:ss");
+        final Date date = new Date(System.currentTimeMillis());
+        return df.format(date);
     }
 }
